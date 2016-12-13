a14da610f0743bad506cbb32f7ead10713886de5
mosoedwsync <Home@Erics-iMac.local>
2016-12-13 00:02:45 -0500

    0

--- standup.py
+++ standup.py
@@ -90,7 +90,7 @@ def done():
     time.append(datetime.datetime.now())
     standup_time()
     tabled()
-    post_message('Nice job team. Please update status of your tasks in JIRA here %s \n Bye!' % jiraurl)
+    post_message('Nice job team. Please update status of your tasks in JIRA here %s \n Bye!' % jira_url)
     reset()
     
 def reset():
